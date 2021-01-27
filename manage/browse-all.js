a3dc643e2e934ade9a8479572dbcb13f0bd2e10b
Giuseppe Zizza <51428014+ZizzaG@users.noreply.github.com>
2021-01-27 14:52:38 -0500

    Update browse-all.js

--- manage/browse-all.js
+++ manage/browse-all.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.api.resources()
-  .then(result => console.log(result))
+cloudinary.api.resources({
+  max_results: '100'
+})
+  .then(result => {
+    console.log(result)
+    console.log("Count:", result.resources.length)
+  })
   .catch(error => console.error(error));
